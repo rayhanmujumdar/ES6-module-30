6623d6ab88423dce9bfc03496655a4bfb2d3facb
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-20 00:56:03 +0600

    let or const using concept added

---
+++ let-const.js
@@ -0,0 +1,14 @@
+const balance = 1230;
+balance = 1450; //not use;beacuse constant varriable is one time delire;
+const userName = "khalid farhan";
+const weTogether = "ami" + userName;//const varriable work using but don"t changing;
+
+// const varriabl use to arrayl
+const numbers = [20,30,50,60];
+// numbers = [99,145,3]//not allwed;beacuse const varrable not changes;
+numbers = [99,145,2];//allow
+numbers[1] = 333;//allow
+const student = {roll: 101,name: 'mofij',job: "intern"};
+student.roll = 86;//allowed;
+student = {name:"sbbbir"}//not allowed;
+// if i change my varrable i must use to let varrable;
