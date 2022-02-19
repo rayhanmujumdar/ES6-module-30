6b971d24db548e6ec838c8a2f17a1968d78fdb4e
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-20 01:49:49 +0600

    templete-string added

---
+++ templete-string.js
@@ -0,0 +1,28 @@
+const priya = 'Asif Akbar';
+const meye = "meye tumi ki dukkho chino";
+const kobita = `kobita tumi sopno charini`;
+const multiLine = 'this is my first line \n'
++'this is my first link\n'
++"sabbir tumi kothay"//old type of delire string; 
+// es6 version using this;
+const multiLineNew = `this is a multiline
+this is second line
+this is third line
+fourth line
+`;
+
+const Count = 5;
+const old = "<h3 class='friend-name'>Friend-2</h3>"
+const old2 = "<h3 class='friend-name'>Friend-"+Count+"</h3>"
+console.log(old2)
+// tampate style es6;
+const new1 = `<h3 class='friend-name'>Friend-${Count}</h3>`
+console.log(new1);
+const firstName = "rakib";
+const lastName = "ahmed";
+const fullold = firstName + " " + lastName; //full pera and distrap code;
+// tampate style ES6;
+const fullMyName = `This person name is: ${firstName} ${lastName}`;//easyly handle no pera;
+
+console.log(fullMyName)
+
